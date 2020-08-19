a7b68c966c44c2541a6650989c21605c24910110
hjmr <2620990+hjmr@users.noreply.github.com>
2020-08-19 14:13:59 +0900

    fix a bug.

--- dataset_jp_text.py
+++ dataset_jp_text.py
@@ -6,16 +6,16 @@ from utils.bert import BertTokenizer, load_vocab
 
 
 class FieldSet():
-    def __init__(self, vocab_file, max_text_length=256, mecab_dict=None):
-        self.tokenizer = BertTokenizer(vocab_file=vocab_file, do_lower_case=True)
+    def __init__(self, vocab_file, max_text_length=256, use_basic_form=False, mecab_dict=None):
+        self.tokenizer = BertTokenizer(vocab_file=vocab_file, do_lower_case=False, do_basic_tokenize=False)
         if mecab_dict is not None:
             self.tagger = MeCab.Tagger("-d {}".format(mecab_dict))
         else:
             self.tagger = MeCab.Tagger("")
-        self.text, self.label = self._prepare(max_text_length)
+        self.text, self.label = self._prepare(max_text_length, use_basic_form)
         self.vocab, self.ids_to_tokens = self._load_vocab(vocab_file)
 
-    def _prepare(self, max_text_length):
+    def _prepare(self, max_text_length, use_basic_form):
         def _wakati_jp_text(text):
             self.tagger.parse("")  # to avoid bug
 
@@ -23,13 +23,15 @@ class FieldSet():
             token = self.tagger.parseToNode(text.strip())
             while token:
                 features = token.feature.split(",")
-                if features[0] == "記号":
-                    if features[1] == "句点":
-                        word_list.append(".")
-                    elif features[1] == "読点":
-                        word_list.append(",")
-                    else:
+                if features[0] == "記号" and features[1] == "句点":
+                    word_list.append(".")
+                elif features[0] == "記号" and features[1] == "読点":
+                    word_list.append(",")
+                else:
+                    if use_basic_form:
                         word_list.append(features[6] if 0 < len(features[6]) else token.surface)
+                    else:
+                        word_list.append(token.surface)
                 token = token.next
             wakati = " ".join(word_list)
             return wakati
@@ -45,7 +47,7 @@ class FieldSet():
             fix_length=max_text_length,
             preprocessing=None,
             postprocessing=None,
-            lower=True,
+            lower=False,
             tokenize=tokenize_jp,
             include_lengths=True,
             init_token="[CLS]",
@@ -85,6 +87,9 @@ def get_data_loader(data_set, batch_size=16, for_train=False):
 
 
 if __name__ == "__main__":
+    import warnings
+    warnings.filterwarnings("ignore")
+
     import argparse
 
     def parse_arg():
@@ -92,6 +97,7 @@ if __name__ == "__main__":
         parser.add_argument("--mecab_dict", type=str, help="MeCab dictionary.")
         parser.add_argument("--batch_size", type=int, default=16, help="batch size.")
         parser.add_argument("--text_length", type=int, default=256, help="the length of texts.")
+        parser.add_argument("--index", type=int, default=1, help="index of text to be shown.")
         parser.add_argument("train_tsv", type=str, nargs=1, help="TSV file for train data.")
         parser.add_argument("test_tsv", type=str, nargs=1, help="TSV file for test data.")
         parser.add_argument("vocab_file", type=str, nargs=1, help="a vocabulary file.")
@@ -99,7 +105,7 @@ if __name__ == "__main__":
 
     args = parse_arg()
 
-    field_set = FieldSet(args.vocab_file[0], args.text_length, args.mecab_dict)
+    field_set = FieldSet(args.vocab_file[0], args.text_length, mecab_dict=args.mecab_dict)
 
     train_validation_ds = load_data_set(args.train_tsv[0], field_set)
     train_ds, validation_ds = train_validation_ds.split(split_ratio=0.8)
@@ -114,7 +120,7 @@ if __name__ == "__main__":
     print(batch.Label)
 
     # ミニバッチの1文目を確認してみる
-    text_minibatch_1 = (batch.Text[0][1]).numpy()
+    text_minibatch = (batch.Text[0][args.index]).numpy()
     # IDを単語に戻す
-    text = field_set.tokenizer.convert_ids_to_tokens(text_minibatch_1)
+    text = field_set.tokenizer.convert_ids_to_tokens(text_minibatch)
     print(text)
