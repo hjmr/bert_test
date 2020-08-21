08bb44c38ad72245f23dde358f837912e01f42bb
hjmr <2620990+hjmr@users.noreply.github.com>
2020-08-21 15:02:24 +0900

    add a test script for IMDb datasets.

---
+++ run_test_IMDb.sh
@@ -0,0 +1,16 @@
+BATCH_SIZE=32
+TEXT_LENGTH=256
+
+MODEL_DIR=./models/ermk_model
+CONF_FILE=${MODEL_DIR}/bert_config.json
+VOCAB_FILE=${MODEL_DIR}/bert-base-uncased-vocab.txtbert-base-uncased-vocab.txt
+
+TRAINED_MODEL=./results/IMDb/net_trained.pth
+TEST_TSV=./data/masuda/IMDb_test.tsv
+
+
+function run_once() {
+    poetry run python test.py --IMDb --batch_size ${BATCH_SIZE} --text_length ${TEXT_LENGTH}   ${CONF_FILE}  ${TRAINED_MODEL}  ${TEST_TSV}  ${VOCAB_FILE}
+}
+
+run_once
